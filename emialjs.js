678443ddd4550c7bd6b996d8697e7155f9de75d5
Shebil K <129933092+shebilmuhammed333@users.noreply.github.com>
2023-04-06 17:02:31 +1200

    Add files via upload

---
+++ emialjs.js
@@ -0,0 +1,26 @@
+function sendMail(e){
+    event.preventDefault();
+    console.log(document.getElementById('name').value)
+    console.log(document.getElementById('email').value)
+    console.log(document.getElementById('msg').value)
+    console.log(document.getElementById('subject').value)
+    let loading=document.querySelector('.loading');
+
+
+
+var params={
+        
+    from_name:document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    msg: document.getElementById('msg').value,
+    subject:document.getElementById('subject').value
+    
+    
+}
+emailjs.send("service_q7v2ymr","template_kebqh8u",params).then(function(res){
+    
+alert(" message send sucsessfully"+res.status)})
+
+
+
+}
